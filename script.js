6b37fc3316fad5dad5b1098ccb567bfb49d4d604
Luis Guilherme <guilherme32698@gmail.com>
2025-10-10 15:41:24 -0300

    Inserindo função para trocar a cor de fundo do card

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+// input elements
 const inputName = document.getElementById('name-input')
 const selectColor = document.getElementById('color-select');
 const selectBorder = document.getElementById('border-select');
 const selectAvatar= document.getElementById('avatar-select');
 
+//card elements
 const cardProfile = document.getElementById('profile-card');
 const avatarCard = document.getElementById('card-avatar');
 const nameCard = document.getElementById('card-name');
 const bioCard = document.getElementById('card-bio');
+const bioInput = document.getElementById('bio-input');
 
 inputName.addEventListener("input", (evento) => {
     nameCard.textContent = inputName.value;
-}) ;
+}) ; // pegando o valor do input e colocando no card
+
+bioInput.addEventListener("input", (evento) => {
+    bioCard.textContent = bioInput.value;
+    
+}); // pegando o valor do input e colocando no card
+
+selectColor.addEventListener("change", (evento) => {
+    cardProfile.style.backgroundColor = selectColor.value;
+}); // alterando a cor do card
+
+selectBorder.addEventListener("change", (evento) => {
+    cardProfile.style.border = selectBorder.value;
+}); // alterando a borda do card
